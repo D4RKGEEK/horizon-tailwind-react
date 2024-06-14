4fa76a55704d9b05567e14cddd406d2bb6047ec4
D4RKGEEK <vaibhav97825@gmail.com>
2024-06-14 09:54:45 +0530

    init

--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -4,7 +4,7 @@ import Notification from "./components/Notification";
 import Project from "./components/Project";
 import Storage from "./components/Storage";
 import Upload from "./components/Upload";
-
+import React from "react";
 const ProfileOverview = () => {
   return (
     <div className="flex w-full flex-col gap-5">
